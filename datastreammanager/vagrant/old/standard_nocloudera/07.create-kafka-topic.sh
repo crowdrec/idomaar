bbc4a73160aa87d266d52df3e206ca1326eb7b76
davidemalagoli <davide.malagoli@moviri.com>
2015-01-23 19:44:26 +0100

    removed old data

--- datastreammanager/vagrant/old/standard_nocloudera/07.create-kafka-topic.sh
+++
@@ -1,3 +0,0 @@
-
-/opt/apache/kafka/bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic data  --partitions 1 --replication-factor 1
-/opt/apache/kafka/bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic recommendations  --partitions 1 --replication-factor 1
