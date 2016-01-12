e1989f90ec2003c7f98d64858414026b2916124e
András Serény <andras.sereny@gmail.com>
2016-01-12 11:54:58 +0100

    Work around aws plugin bug, downgrade ZK as no 3.4.7 is available, #64

--- datastreammanager/vagrant/03.zookeeper.sh
+++ datastreammanager/vagrant/03.zookeeper.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -Eux
 
 cd /tmp
-wget -nv -O zookeeper.tgz http://www.eu.apache.org/dist/zookeeper/stable/zookeeper-3.4.7.tar.gz
+wget -nv -O zookeeper.tgz http://www.eu.apache.org/dist/zookeeper/stable/zookeeper-3.4.6.tar.gz
 
 tar -xvf zookeeper.tgz
 mkdir -p /opt/apache
-mv zookeeper-3.4.7 /opt/apache
+mv zookeeper-3.4.6 /opt/apache
 cd /opt/apache
-ln -s /opt/apache/zookeeper-3.4.7 zookeeper
+ln -s /opt/apache/zookeeper-3.4.6 zookeeper
 
 cp /vagrant/vagrant/init.d/zookeeper-server /etc/init.d/
 cp /opt/apache/zookeeper/conf/zoo_sample.cfg /opt/apache/zookeeper/conf/zoo.cfg 
@@ -16,7 +16,7 @@ mkdir /var/log/zookeeper
 
 useradd zookeeper
 chown -R zookeeper:zookeeper /opt/apache/zookeeper
-chown -R zookeeper:zookeeper /opt/apache/zookeeper-3.4.7
+chown -R zookeeper:zookeeper /opt/apache/zookeeper-3.4.6
 chown -R zookeeper:zookeeper /var/log/zookeeper
 
 service zookeeper-server start
